125086f54d5beac8c04a7a541a42001538c2c636
agungpens <agung20.mhs@akb.ac.id>
2023-03-02 09:18:33 +0700

    Handling Request

--- server.js
+++ server.js
@@ -4,8 +4,23 @@ const requestListener = (request, response) => {
     response.setHeader('Content-Type', 'text/html');
 
     response.statusCode = 200;
-    response.end('<h1>Hello, HTTP Server!</h1>');
-}
+    // Handling Request
+    const { method } = request;
+
+    if (method == "GET") {
+        response.end('<h1>Hello, HTTP Server GET!</h1>');
+    }
+    if (method == "POST") {
+        response.end('<h1>Hello, HTTP Server POST!</h1>');
+    }
+    if (method == "PUT") {
+        response.end('<h1>Hello, HTTP Server PUT!</h1>');
+    }
+    if (method == "DELETE") {
+        response.end('<h1>Hello, HTTP Server DELETE!</h1>');
+    }
+
+};
 
 const server = http.createServer(requestListener);
 const port = 5000;
